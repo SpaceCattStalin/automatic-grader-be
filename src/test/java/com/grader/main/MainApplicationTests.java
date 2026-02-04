b1fd3397d99c98bc836153f74433de1c6cc91d37
SpaceCattStalin <spacestalincyka@gmail.com>
2026-02-04 20:16:27 +0700

    Create project

---
+++ src/test/java/com/grader/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grader.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
